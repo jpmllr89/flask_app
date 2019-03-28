5473e88f6d615e4f7cbc54c9ccf7118491d26e51
jpmllr89 <jpmllr89@gmail.com>
2019-03-28 12:44:51 -0400

    added a generator function

--- functions.py
+++ functions.py
@@ -1,12 +1,15 @@
 students = []
 
+
 def get_students_titlecase():
-    students_titlecase = [student['name'].title() for student in students]
+    students_titlecase.append([student['name'].title() for student in students])
     return students_titlecase
+
 
 def print_students_titlecase():
     students_titlecase = get_students_titlecase()
     print(students_titlecase)
+
 
 def add_student(name, student_id = 0):
     student = {
@@ -16,3 +19,28 @@ def add_student(name, student_id = 0):
         
         }
     students.append(name)
+
+
+def save_file(student):
+    try:
+        f = open("students.txt", "a")
+        f.write(student + "\n")
+        f.close()
+    except Exception:
+        print("Could not save file")
+
+
+def read_students(f):
+    for line in f:
+        yield line
+
+def read_file():
+    try:
+        f = open("students.txt", "r")
+        for student in read_students(f):
+            add_student(student)
+        f.close()
+    
+    except Exception:
+        print("Could not read file")
+
