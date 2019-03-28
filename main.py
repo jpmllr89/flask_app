cede2f9de9b3620cac6f11256a75f8463ac14a15
jpmllr89 <jpmllr89@gmail.com>
2019-03-28 18:29:19 -0400

    updated index.htmlfile, then addeda route for the students list

--- main.py
+++ main.py
@@ -14,13 +14,16 @@ def students_page():
         new_student_name = request.form.get("name", "")
         new_student_last_name = request.form.get("last-name", "")
 
-        new_student = Student(name=new_student_name, student_id=new_student_id)
+        new_student = Student(name=new_student_name, last_name = new_student_last_name, student_id=new_student_id)
         students.append(new_student)
 
         return redirect(url_for("students_page"))
     return render_template("index.html", students=students)
-def hello():
-    return "Hello World!"
+@app.route("/student_list", methods = ['GET', 'POST'])
+def students_list():
+    if request.method=="GET":
+        return redirect(url_for("students_list"))
+    return render_template("students.html", students = students)
 
 if __name__ == "__main__":
     app.run(debug = True)
--- student.py
+++ student.py
@@ -1,15 +1,18 @@
 
+students = []
+
 
 class Student:
-    students = []
     _school_name = "Springfield Elementary"
 
-    def __init__(self, name, student_id = 0):
+    def __init__(self, name, last_name, student_id = 0):
         self.name = name
+        self.last_name = last_name
         self.student_id = student_id
         student = {
         
             "name": self.name, 
+            "last_name": self.last_name,
             "student_id": self.student_id
         
             }
