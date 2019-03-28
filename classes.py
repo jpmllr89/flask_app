3d6666c86b274ee54ecd167503cf1dbe2216898c
jpmllr89 <jpmllr89@gmail.com>
2019-03-28 13:26:27 -0400

    added another method to student class, and tested it.

--- classes.py
+++ classes.py
@@ -2,17 +2,29 @@
 students = []
 
 class Student:
+
+
     def __init__(self, name, student_id = 0):
+        self.name = name
+        self.student_id = student_id
         student = {
         
-            "name":name, 
-            "student_id": student_id
+            "name": self.name, 
+            "student_id": self.student_id
         
             }
         students.append(student)
 
-    def __str__(self):
-        return f"Student: {self.name}"
 
-student = Student()
+    def __str__(self):
+        return f"Student: {self.get_name_capitalized()}"
+    
+
+    def get_name_capitalized(self):
+        return self.name.capitalize()
+
+student = Student("park")
+
+print(student.get_name_capitalized())
+print(student.__str__())
 
